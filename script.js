5ab6baa73e7797ba7219f4e8743533fe69ecc541
Ogarri <ba.morlion@gmail.com>
2025-12-02 16:26:38 +0100

    getion des parties et de la reinitialisation

--- script.js
+++ script.js
@@ -34,9 +34,49 @@ class Grille {
     }
 }
 
+class Score {
+    constructor(manchesGagnantes = 3) {
+        this.scoreJ1 = 0;
+        this.scoreJ2 = 0;
+        this.manchesGagnantes = manchesGagnantes;
+    }
+
+    incrementerJ1() {
+        this.scoreJ1++;
+        this.mettreAJour();
+    }
+
+    incrementerJ2() {
+        this.scoreJ2++;
+        this.mettreAJour();
+    }
+
+    reinitialiser() {
+        this.scoreJ1 = 0;
+        this.scoreJ2 = 0;
+        this.mettreAJour();
+    }
+
+    mettreAJour() {
+        document.getElementById('scoreJ1').textContent = this.scoreJ1;
+        document.getElementById('scoreJ2').textContent = this.scoreJ2;
+    }
+
+    verifierGagnant() {
+        if (this.scoreJ1 >= this.manchesGagnantes) {
+            return 1;
+        } else if (this.scoreJ2 >= this.manchesGagnantes) {
+            return 2;
+        }
+        return 0;
+    }
+}
+
 class Joueur extends Grille{
     constructor(positionDepartX, positionDepartY, couleur) {
         super();
+        this.positionDepartXInitial = positionDepartX;
+        this.positionDepartYInitial = positionDepartY;
         this.positionDepartX = positionDepartX;
         this.positionDepartY = positionDepartY;
         this.couleur = couleur;
@@ -132,6 +172,15 @@ class Joueur extends Grille{
             }
         });
     }
+
+    reinitialiser() {
+        this.positionDepartX = this.positionDepartXInitial;
+        this.positionDepartY = this.positionDepartYInitial;
+        this.directionX = 1;
+        this.directionY = 0;
+        this.positions = [[this.positionDepartXInitial, this.positionDepartYInitial]];
+        this.fileDirections = [];
+    }
 }
 
 var grille = new Grille();
@@ -139,15 +188,115 @@ grille.dessinerGrille();
 
 var joueur1 = new Joueur(1, 28, "blue");
 var joueur2 = new Joueur(1, 30, "red");
-var jeuEnCours = true;
-var intervalId;
+var score = new Score(3);
+var jeuEnCours = false;
+var partieCommencee = false;
+var intervalId = null;
+
+const btnDemarrer = document.getElementById('btnDemarrer');
+const btnReinitialiser = document.getElementById('btnReinitialiser');
+
+function verifierFinPartie() {
+    const gagnant = score.verifierGagnant();
+    if (gagnant === 1) {
+        afficherGagnant("Joueur 1 (Bleu) remporte la partie!");
+        btnDemarrer.disabled = false;
+        btnDemarrer.textContent = "Nouvelle partie";
+        partieCommencee = false;
+        return true;
+    } else if (gagnant === 2) {
+        afficherGagnant("Joueur 2 (Rouge) remporte la partie!");
+        btnDemarrer.disabled = false;
+        btnDemarrer.textContent = "Nouvelle partie";
+        partieCommencee = false;
+        return true;
+    }
+    return false;
+}
+
+function lancerNouvelleManche() {
+    joueur1.reinitialiser();
+    joueur2.reinitialiser();
+    jeuEnCours = true;
+    dessinerTout();
+}
+
+function demarrerPartie() {
+    score.reinitialiser();
+    joueur1.reinitialiser();
+    joueur2.reinitialiser();
+    jeuEnCours = true;
+    partieCommencee = true;
+    btnDemarrer.disabled = true;
+    btnDemarrer.textContent = "Partie en cours...";
+    dessinerTout();
+    
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
+    
+    intervalId = setInterval(() => {
+        if (!jeuEnCours) return;
+        
+        joueur1.avancer();
+        joueur2.avancer();
+        dessinerTout();
+        
+        let collision1 = joueur1.verifierCollisionBord() || joueur1.verifierCollision(joueur2);
+        let collision2 = joueur2.verifierCollisionBord() || joueur2.verifierCollision(joueur1);
+        
+        if (collision1 || collision2) {
+            jeuEnCours = false;
+            
+            if (collision1 && collision2) {
+                dessinerTout();
+                ctx.fillStyle = "white";
+                ctx.font = "32px Arial";
+                ctx.textAlign = "center";
+                ctx.textBaseline = "middle";
+                ctx.fillText("Égalité!", CANVAS.width / 2, CANVAS.height / 2);
+            } else if (collision1) {
+                score.incrementerJ2();
+            } else if (collision2) {
+                score.incrementerJ1();
+            }
+            
+            setTimeout(() => {
+                if (!verifierFinPartie()) {
+                    lancerNouvelleManche();
+                }
+            }, 1500);
+        }
+    }, 100);
+}
+
+function reinitialiserPartie() {
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
+    
+    jeuEnCours = false;
+    partieCommencee = false;
+    score.reinitialiser();
+    joueur1.reinitialiser();
+    joueur2.reinitialiser();
+    
+    btnDemarrer.disabled = false;
+    btnDemarrer.textContent = "Démarrer";
+    
+    ctx.clearRect(0, 0, CANVAS.width, CANVAS.height);
+    dessinerTout();
+}
+
+btnDemarrer.addEventListener('click', demarrerPartie);
+btnReinitialiser.addEventListener('click', reinitialiserPartie);
 
 function afficherGagnant(gagnant) {
     ctx.fillStyle = "white";
     ctx.font = "48px Arial";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
-    ctx.fillText(`${gagnant} a gagné!`, CANVAS.width / 2, CANVAS.height / 2);
+    ctx.fillText(gagnant, CANVAS.width / 2, CANVAS.height / 2);
 }
 
 function dessinerTout() {
@@ -157,35 +306,8 @@ function dessinerTout() {
 }
 dessinerTout();
 
-intervalId = setInterval(() => {
-    if (!jeuEnCours) return;
-    
-    joueur1.avancer();
-    joueur2.avancer();
-    dessinerTout();
-    
-    if (joueur1.verifierCollisionBord()) {
-        jeuEnCours = false;
-        clearInterval(intervalId);
-        afficherGagnant("Joueur 2 (Rouge)");
-    } else if (joueur2.verifierCollisionBord()) {
-        jeuEnCours = false;
-        clearInterval(intervalId);
-        afficherGagnant("Joueur 1 (Bleu)");
-    }
-    else if (joueur1.verifierCollision(joueur2)) {
-        jeuEnCours = false;
-        clearInterval(intervalId);
-        afficherGagnant("Joueur 2 (Rouge)");
-    } else if (joueur2.verifierCollision(joueur1)) {
-        jeuEnCours = false;
-        clearInterval(intervalId);
-        afficherGagnant("Joueur 1 (Bleu)");
-    }
-}, 100);
-
 document.addEventListener('keydown', function(event) {
-    if (!jeuEnCours) return;
+    if (!jeuEnCours || !partieCommencee) return;
     
     switch(event.key) {
         case 'd':
@@ -216,7 +338,5 @@ document.addEventListener('keydown', function(event) {
 });
 
 /*Reste à faire :
-- Ajouter une touche pour chaque joueur qui permet de sauter par dessus un obstacle (une seule case). Le tracé recouvre l’obstacle et continue au-delà de l’obstacle, si c’est possible.
-- Une partie se joue en trois manches gagnantes. Dès qu’un joueur heurte un obstacle, la manche suivante se lance immédiatement. Il peut y avoir égalité sur une manche, dans ce cas le score n’évolue pas. Afficher le score (en dehors du canvas).
-- Ajouter un bouton pour démarrer/redémarrer une nouvelle partie. Ce bouton est désactivé lorsque la partie est en cours.
+- Ajouter une touche pour chaque joueur qui permet de sauter par dessus un obstacle (une seule case). Le tracé recouvre l'obstacle et continue au-delà de l'obstacle, si c'est possible.
 - Ajouter un bouton pour permettre aux joueurs de configurer les touches de direction. Ceci devra se faire dans une fenêtre modale (utiliser par exemple la fonction dialog() de Jquery UI). */
